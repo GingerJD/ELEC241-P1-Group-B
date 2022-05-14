6d79075d7d3d4a81676d9575f7834327214e7eaf
Jamie Davis <james.davis-11@students.plymouth.ac.uk>
2022-05-14 17:46:29 +0100

    [14/05/22] DC finished
    > DC should be finished however needs testing on the LCD and probably minor changes
    > PWMC has been changed to only update duty cycle and period at the end of the period
    > PWMC testbench will need changing to accurately test the PWMC
    >DC still needs a testbench
    >ATU, SCU,DC,PWMC and their respective testbenches need neatening and commenting before final submission

--- FPGA/display_controller.sv
+++ FPGA/display_controller.sv
@@ -6,11 +6,12 @@ module display_controller (
    input logic [7:0] ascii_data,
    input logic write,
    input logic clk,
+   input logic busyF,
    input logic [11:0] angle);
 
 //Internal Variables
 reg [11:0] updateAngle;
-reg [24:0] refreshCount;
+reg [21:0] refreshCount;
 reg [11:0] degrees;
 //BCD CONVERSION
 reg [11:0] BCD;
@@ -25,31 +26,74 @@ always @(posedge clk)begin
 	end
 end
 
-always_latch begin
-////////////////////////////////////////////////////////////////////////////////
-//CONVERT ANGLES TO DEGREES
-degrees = (updateAngle * 360)/1006;
-if(((updateAngle * 360)-(degrees*1006))>503)	//Round to nearest degree
-		degrees = degrees +1;
-
-////////////////////////////////////////////////////////////////////////////////
-//CONVERT DEGREES TO BCD
-BCD [11:8] = degrees / 100;
-BCD [7:4] = (degrees-(BCD [11:8] * 100))/10;
-BCD [3:0] = (degrees-(BCD [11:8] * 100)-(BCD [7:4]*10));
-
-////////////////////////////////////////////////////////////////////////////////
-//CONVERT BCD TO ASCII
-asciiConversion [24:17] = BCD [11:8] + 48;
-asciiConversion [16:8] = BCD [7:4] + 48;
-asciiConversion [7:0] = BCD [3:0] + 48;
-
-////////////////////////////////////////////////////////////////////////////////
-//DISLAY ASCII ON LCD
 
 
+always @(updateAngle)begin
+	//DISLAY ASCII ON LCD
+	//Read busy flag
+	rs = 0;
+	rw = 1;
+	@(negedge busyF)begin
+		//clear LCD
+		e = 0;
+		rs = 0;
+		rw = 0;
+		data = 7'b0000001;
+		e = 1;
+	end
+	rs = 0;
+	rw = 1;
+	@(negedge busyF)begin
+		//write hundreds
+		e = 0;
+		rs = 1;
+		rw = 0;
+		data = asciiConversion [20:14];
+		e = 1;
+	end
+	rs = 0;
+	rw = 1;
+	@(negedge busyF)begin
+		//write tens
+		e = 0;
+		rs = 1;
+		rw = 0;
+		data = asciiConversion [13:7];
+		e = 1;
+	end
+	rs = 0;
+	rw = 1;
+	@(negedge busyF)begin
+		//write ones
+		e = 0;
+		rs = 1;
+		rw = 0;
+		data = asciiConversion [6:0];
+		e = 1;
+	end
 end
 
-   
+
+
+always_latch begin
+	////////////////////////////////////////////////////////////////////////////////
+	//CONVERT ANGLES TO DEGREES
+	degrees = (updateAngle * 360)/1006;
+	if(((updateAngle * 360)-(degrees*1006))>503)	//Round to nearest degree
+			degrees = degrees +1;
+	
+	////////////////////////////////////////////////////////////////////////////////
+	//CONVERT DEGREES TO BCD
+	BCD [11:8] = degrees / 100;
+	BCD [7:4] = (degrees-(BCD [11:8] * 100))/10;
+	BCD [3:0] = (degrees-(BCD [11:8] * 100)-(BCD [7:4]*10));
+	
+	////////////////////////////////////////////////////////////////////////////////
+	//CONVERT BCD TO ASCII
+	asciiConversion [20:14] = BCD [11:8] + 48;
+	asciiConversion [13:7] = BCD [7:4] + 48;
+	asciiConversion [6:0] = BCD [3:0] + 48;
+	
+end   
 endmodule
    
--- FPGA/pmw_controller.sv
+++ FPGA/pmw_controller.sv
@@ -25,19 +25,27 @@ module pwmc(output logic motor_1, motor_2,
 //internal variables
 int counter;
 int updateWait = 0;
+logic [7:0] dutyCycleUpdate;
+logic [7:0] periodUpdate;
+
+//Wait for period to end before updating duty cycle and period
+always @(posedge motor_1 or motor_2)begin
+	dutyCycleUpdate = dutyCycle;
+	periodUpdate = period;
+end
 
 //COUNTER TO DETERMINE WHEN PULSE SHOULD BE HIGH
 always @(posedge clk_50)begin
 	if (updateWait == 1)
 		counter = 0;
-	if ((counter < period-1)&&(pwmOutEnable == 1))
+	if ((counter < periodUpdate-1)&&(pwmOutEnable == 1))
 		counter = counter +1;
 	else
 		counter = 0;
 end
 
 //IF PERIOD OR DUTY CYCLE UPDATED RESTART COUNTER
-always @(period, dutyCycle)begin
+always @(periodUpdate, dutyCycleUpdate)begin
 		updateWait = 1;	//if Period or duty cycle are updated wait until next posedge of clock to begin pwm
 		@(posedge clk_50)begin
 			updateWait = 0;
@@ -60,13 +68,13 @@ always_latch begin
 		else if((pwmOutEnable == 1)&&(brake != 1))begin
 			//motor direction = clockwise
 			if (direction == 1)begin
-				motor_2 = (counter < dutyCycle) ? 1:0;
+				motor_2 = (counter < dutyCycleUpdate) ? 1:0;
 				motor_1 = 0;
 			end
 			//motor direction = anticlockwise
 			else if (direction == 0)begin
 				motor_2 = 0;
-				motor_1 = (counter < dutyCycle) ? 1:0;
+				motor_1 = (counter < dutyCycleUpdate) ? 1:0;
 			end
 		end
 		//Pulls motors low when pwm outputs are disabled
