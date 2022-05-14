1473d17aee0d8e113265a3b90bc9af1e636fa06b
Jamie Davis <james.davis-11@students.plymouth.ac.uk>
2022-05-14 16:46:12 +0100

    [14/05/22] DC progress
    > DC reads angles at fixed rate of 60Hz
    > DC converts angles to rounded degrees
    > DC converts rounded degrees to BCD
    > DC converts BCD to 3 ascii characters stored in asciiConversion register
    > DC still needs display to LCD implementation
    > DC still needs testbench
    I should have DC and testbench finished today

--- FPGA/display_controller.sv
+++ FPGA/display_controller.sv
@@ -5,8 +5,50 @@ module display_controller (
    output logic e,
    input logic [7:0] ascii_data,
    input logic write,
-   input logic clk);
+   input logic clk,
+   input logic [11:0] angle);
 
+//Internal Variables
+reg [11:0] updateAngle;
+reg [24:0] refreshCount;
+reg [11:0] degrees;
+//BCD CONVERSION
+reg [11:0] BCD;
+//ASCII CONVERSION
+reg [24:0] asciiConversion;
+
+always @(posedge clk)begin
+	refreshCount++;
+	if (refreshCount == 16666667)begin	//Read angle at 60Hz
+		updateAngle = angle;
+		refreshCount = 0;
+	end
+end
+
+always_latch begin
+////////////////////////////////////////////////////////////////////////////////
+//CONVERT ANGLES TO DEGREES
+degrees = (updateAngle * 360)/1006;
+if(((updateAngle * 360)-(degrees*1006))>503)	//Round to nearest degree
+		degrees = degrees +1;
+
+////////////////////////////////////////////////////////////////////////////////
+//CONVERT DEGREES TO BCD
+BCD [11:8] = degrees / 100;
+BCD [7:4] = (degrees-(BCD [11:8] * 100))/10;
+BCD [3:0] = (degrees-(BCD [11:8] * 100)-(BCD [7:4]*10));
+
+////////////////////////////////////////////////////////////////////////////////
+//CONVERT BCD TO ASCII
+asciiConversion [24:17] = BCD [11:8] + 48;
+asciiConversion [16:8] = BCD [7:4] + 48;
+asciiConversion [7:0] = BCD [3:0] + 48;
+
+////////////////////////////////////////////////////////////////////////////////
+//DISLAY ASCII ON LCD
+
+
+end
 
    
 endmodule
--- FPGA/test.sv
+++ FPGA/test.sv
@@ -1,19 +1,40 @@
-module test(output logic [11:0] a, output logic [11:0] b, output logic [11:0] acwDiff, output logic [11:0] cwDiff, output logic direction);
+module test(output logic [11:0] a, output logic [11:0] atuAngle,output logic [11:0] BCD, output logic [3:0] hundreds, output logic [3:0] tens,output logic [3:0] ones,output logic [11:0] degrees, output logic direction);
 initial begin
 
 logic [7:0] pwmDT;
 logic [7:0] pwmPeriod;
-logic [11:0] atuAngle;
+//logic [11:0] atuAngle;
 logic [11:0] desiredAngle;
 int x = 1006;
 int y = 296;
 
-a = x/y;
 
-b = x-(a*y);
-if(b >= (y/2))begin
-	a = a+1;
-end
+
+atuAngle = 995;
+degrees = (atuAngle * 360)/1006;
+if(((atuAngle * 360)-(degrees*1006))>503)
+		degrees = degrees +1;
+#100
+$display("T1");
+hundreds = (degrees / 100);
+tens = (degrees-(hundreds * 100))/10;
+ones = (degrees-(hundreds * 100)-(tens*10));
+$display("T2");
+
+BCD [11:8] = degrees / 100;
+BCD [7:4] = (degrees-(BCD [11:8] * 100))/10;
+BCD [3:0] = (degrees-(BCD [11:8] * 100)-(BCD [7:4]*10));
+
+
+
+
+
+
+//
+//b = x-(a*y);
+//if(b >= (y/2))begin
+//	a = a+1;
+//end
 //desiredAngle = 978;
 //atuAngle = 28;
 //
